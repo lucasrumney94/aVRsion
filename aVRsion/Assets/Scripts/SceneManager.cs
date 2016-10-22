26b5091e4c789cdbf0d932d3635f85f501989bc0
Xueer Zhu <zhuxueerwhitney@gmail.com>
2016-10-22 12:56:36 -0400

    Add SceneManager script

---
+++ aVRsion/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManager : MonoBehaviour {
+	//Declare SceneManager as a singleton
+	public static SceneManager instance = null;
+
+	[SerializeField]
+	private int level = 0; 
+
+	void Awake() 
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		} else if (instance != this) 
+		{
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+	}
+		
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		// get input and shit
+
+		Debug.Log ("Safe Zone Button Pressed!");
+	}
+}
